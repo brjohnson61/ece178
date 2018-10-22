2c01a4493aa66a5a69f899a203a77c59e75da257
Blake Johnson <brjohnson61@gmail.com>
2018-10-22 11:45:07 -0700

    finished 1

--- hw3/hw3pt1.m
+++ hw3/hw3pt1.m
@@ -1,49 +1,79 @@
 
 image = imread("image1.jpg");
-newImage = image;
+
 [rows, columns, colorChannels] = size(image);
 
-function halftone(image, rowBegin, rowEnd, columnBegin, columnEnd)
-    rowMid = 0;
-    if(rowEnd-rowBegin > 1)
-        rowMid = round((rowEnd-rowBegin)/2);
-    else
-        rowMid = rowEnd;
+newImageBin = binary(image);
+newImageHalftone = halftone(image, 0);
+
+figure, imshow(image);
+figure, imshow(newImageBin);
+figure, imshow(newImageHalftone);
+
+function J = binary(image)
+    [rows, columns, colorChannels] = size(image);
+    for i=1:rows
+       for j=1:columns
+           if(image(i,j) > 128)
+              image(i,j) = 255;
+           else
+              image(i,j) = 0;
+           end
+       end
     end
-    columnMid = 0;
-    if(columnEnd - columnBegin > 1)
-        columnMid = round((columnEnd-columnBegin)/2);
+    J=image;
+end
+
+function J = halftone(image, is3x3)
+    [rows, columns, colorChannels] = size(image);
+    if(is3x3)
+        stepSize = 3;
     else
-        columnMid = columnEnd;
+        stepSize = 2;
     end
-        
-    if(columnEnd-columnBegin == 1 && rowEnd-rowBegin == 1)
-        total = 0;
-        for i=columnBegin:columnEnd
-           for j=rowBegin:columnEnd
-               if(image(i,j) > 127)
-                  total = total + image(i,j);
-               else
-                  image(i,j) = 0;
+    for i=1:stepSize:rows
+       for j=1:stepSize:columns
+           total = 0;
+           pixelCount = 0;
+           rowBegin = i;
+           columnBegin = j;
+           rowEnd = rowBegin + stepSize - 1;
+           columnBegin = columnBegin + stepSize - 1;
+           columnEnd = columnBegin + stepSize -1;
+           for k=rowBegin:rowEnd
+               for l=columnBegin:columnEnd
+                   if(k<=rows && l <=columns)
+                       total = total + image(k,l);
+                       pixelCount = pixelCount + 1;
+                   end
                end
-        end
-        
-    elseif(columnEnd-columnBegin == 1 && rowEnd-rowBegin == 0)
-        %equalize 1x2
-        
-    elseif(columnEnd-columnBegin == 0 && rowEnd-rowBegin == 1)
-        %equalize 2x1
-    else
-        halftone(image, rowBegin, round((rowEnd-rowBegin)/2), columnBegin, round((columnEnd-columnBegin)/2));
-        halftone(image, rowBegin, round((rowEnd-rowBegin)/2), round((columnEnd-columnBegin)/2)+1, columnEnd);
-        halftone(image, round((rowEnd-rowBegin)/2)+1, rowEnd, columnBegin, round((columnEnd-columnBegin)/2));
-        halftone(image, round((rowEnd-rowBegin)/2)+1, rowEnd, round((columnEnd-columnBegin)/2)+1, columnEnd);
+           end
+           average = round(total/pixelCount);
+           numWhite = 0;
+           if(average <= 51)
+               numWhite = 0;
+           elseif(average > 51 && average <= 102)
+               numWhite = round(0.25*pixelCount);
+           elseif(average > 102 && average <= 154)
+               numWhite = round(0.5*pixelCount);
+           elseif(average > 154 && average <= 205)
+               numWhite = round(0.75*pixelCount);
+           else
+               numWhite = pixelCount;
+           end
+           for k=rowBegin:rowEnd
+               for l=columnBegin:columnEnd
+                   if(k<=rows && l <=columns)
+                        if(numWhite > 0)
+                           image(k,l) = 255;
+                           numWhite = numWhite - 1;
+                        else
+                           image(k,l) = 0;
+                        end
+                    end
+                end
+           end
+       end
     end
-      
-        
-    
-
-
-
-
+    J=image;
 end
